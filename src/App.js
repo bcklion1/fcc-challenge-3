ef8211106d148ddb9196a802f943f284305943dc
Brian Kunak <BrianKunak@usis.net>
2022-08-10 16:51:48 -0400

    Built UI and started implementation of inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+
+import React, { useState } from "react"
+import { Slider } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
 import './App.css';
 
 function App() {
+  const [display,setDisplay] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flex center">
+      <div class="machine">
+        <div><Noises display={display} setDisplay={setDisplay}/></div>
+        <div><Controls display={display}/></div>
+      </div>
     </div>
   );
 }
+
+function Noises(props){
+  function noisePlayed(note){
+    props.setDisplay(String(note))
+  }
+  return(
+
+    <div class="drum-wrapper">
+      <button onClick={()=>noisePlayed("Q")} class="drum">Q</button>
+      <button onClick={()=>noisePlayed("W")} class="drum">W</button>
+      <button onClick={()=>noisePlayed("E")} class="drum">E</button>
+      <button onClick={()=>noisePlayed("A")} class="drum">A</button>
+      <button onClick={()=>noisePlayed("S")} class="drum">S</button>
+      <button onClick={()=>noisePlayed("D")} class="drum">D</button>
+      <button onClick={()=>noisePlayed("Z")} class="drum">Z</button>
+      <button onClick={()=>noisePlayed("X")} class="drum">X</button>
+      <button onClick={()=>noisePlayed("C")} class="drum">C</button>
+    </div>
+
+  )
+}
+
+function Controls(props){
+  const [volume, setVolume] = React.useState(30);
+  console.log(volume)
+  return(
+    <div class="flex column">
+      <div>
+      <p className="vol">Power</p>
+      <ToggleSwitch label="Volume" />  
+      </div>
+      <div class="screen">{props.display}</div>
+      <div class="slider">
+        <Slider progress defaultValue={30} onChange={(value)=>setVolume(value)} />
+      </div>
+      
+      
+    </div>
+    
+  )
+}
+
+const ToggleSwitch = ({ label }) => {
+  return (
+
+      <div className="toggle-switch">
+        <input type="checkbox" className="checkbox" 
+               name={label} id={label} />
+        <label className="label" htmlFor={label}>
+          <span className="inner" />
+          <span className="switch" />
+        </label>
+      </div>
+
+  );
+};
 
 export default App;
